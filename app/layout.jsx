2fd8b8fddb733a334fce1384daf29fd54ceba05f
israelmitolu <israelmitolu@gmail.com>
2023-07-04 07:37:08 +0100

    Initial commit + authentication done

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metaData = {
+  title: "Promptopia",
+  description: "Promptopia is a place to find AI prompts and share your own.",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
