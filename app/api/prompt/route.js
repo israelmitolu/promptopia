0df6b08e348ed48a12130634e67b423324a5c3c6
israelmitolu <israelmitolu@gmail.com>
2024-02-15 09:46:20 +0100

    fix: Disable ISR (caching)

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,18 @@
 import Prompt from "@models/prompt";
 import { connectToDatabase } from "@utils/database";
 
-export const GET = async (request) => {
+export const revalidate = 0;
+
+export const GET = async (req) => {
   try {
     await connectToDatabase();
-
     const prompts = await Prompt.find({}).populate("creator");
-
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), { status: 201 });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    console.log(error);
+    return new Response(
+      { error: "Failed to fetch all prompts" },
+      { status: 500 }
+    );
   }
 };
